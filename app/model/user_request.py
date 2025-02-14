c3dc3233a9e2c9ed859edca4c9352988b8b1daf2
Anton A Shanaurin <grspectre@gmail.com>
2025-02-14 08:08:39 +0300

    homework 2: Necessary validators + email field

--- app/model/user_request.py
+++ app/model/user_request.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, field_serializer
+from pydantic import BaseModel, Field, field_serializer, EmailStr
 from uuid import UUID, uuid4
 from datetime import date, datetime
 from typing import List
@@ -13,10 +13,11 @@ class RequestReason(str, Enum):
 
 class UserRequest(BaseModel):
     id: UUID = Field(default_factory=uuid4, description="Unique ID")
-    last_name: str = Field(default="LastName", description="Client last name")
-    first_name: str = Field(default="User", description="Client first name")
+    last_name: str = Field(default="LastName", pattern=r"^[A-ZА-ЯЁ][A-zА-яёЁ]+$", description="Client last name")
+    first_name: str = Field(default="User", pattern=r"^[A-ZА-ЯЁ][A-zА-яёЁ]+$", description="Client first name")
     birthday: date = Field(default = date(1970, 1, 1), description="User birthday")
-    phone_number: str | None = Field(default=None, pattern=r"\d{10,15}", description="Phone number")
+    phone_number: str | None = Field(default=None, pattern=r"\d{10,15}", description="User phone number")
+    email: EmailStr | None = Field(default=None, description="User email")
     detection_datetime: datetime = Field(default=datetime.now(), description="Detection datetime")
     reason: List[RequestReason] = Field(default=[])
 
